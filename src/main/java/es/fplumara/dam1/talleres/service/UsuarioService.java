b9c6c6b094f37bac1c31bcc8317c13b2d291f1b9
Samu7526 <samueljosue312005@gmail.com>
2026-02-04 14:19:29 +0100

    Avance-implementacion-Services-Usuario

--- src/main/java/es/fplumara/dam1/talleres/service/UsuarioService.java
+++ src/main/java/es/fplumara/dam1/talleres/service/UsuarioService.java
@@ -1,12 +1,15 @@
 package es.fplumara.dam1.talleres.service;
 
 
+import es.fplumara.dam1.talleres.dto.CrearUsuarioDTO;
 import es.fplumara.dam1.talleres.model.Usuario;
 import java.util.List;
 
 public interface UsuarioService {
 
-    Usuario createUsuario(Usuario CrearUsuario);
+
+
+    Usuario createUsuario(CrearUsuarioDTO datosUsuarioDTO);
 
     List<Usuario> listarusuario();
 
--- src/main/java/es/fplumara/dam1/talleres/service/impl/UsuarioServiceImpl.java
+++ src/main/java/es/fplumara/dam1/talleres/service/impl/UsuarioServiceImpl.java
@@ -1,7 +1,8 @@
 package es.fplumara.dam1.talleres.service.impl;
 
+import es.fplumara.dam1.talleres.dto.CrearUsuarioDTO;
 import es.fplumara.dam1.talleres.model.Usuario;
-import es.fplumara.dam1.talleres.repository.TallerRepository;
+import es.fplumara.dam1.talleres.repository.InscripcionRepository;
 import es.fplumara.dam1.talleres.repository.UserRepository;
 import es.fplumara.dam1.talleres.service.UsuarioService;
 
@@ -9,15 +10,21 @@ import java.util.List;
 
 public class UsuarioServiceImpl implements UsuarioService {
 
-
     private UserRepository UserRepository;
+    private InscripcionRepository inscriptionRepository;
 
-    public UsuarioServiceImpl(UserRepository userRepository) {
-        UserRepository = userRepository;
+    public UsuarioServiceImpl(UserRepository userRepository,  InscripcionRepository inscriptionRepository) {
+        this.UserRepository = userRepository;
+        this.inscriptionRepository = inscriptionRepository;
     }
 
     @Override
-    public Usuario createUsuario(Usuario CrearUsuario) {
+    public Usuario createUsuario(CrearUsuarioDTO datosUsuarioDTO) {
+        // 1-COMPROBAR NOMBRE INFORMADO
+        if (datosUsuarioDTO.getNombre() == null || datosUsuarioDTO.getNombre().isEmpty()) {
+
+        }
+
         return null;
     }
 
