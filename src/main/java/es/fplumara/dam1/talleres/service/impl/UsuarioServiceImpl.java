53f99109aada7158a8766d3dca15e026d3c87def
Jcasillas11 <jcasillas.menendez@gmail.com>
2026-02-04 13:01:02 +0100

    Implementacion

--- src/main/java/es/fplumara/dam1/talleres/service/impl/UsuarioServiceImpl.java
+++ src/main/java/es/fplumara/dam1/talleres/service/impl/UsuarioServiceImpl.java
@@ -1,4 +1,43 @@
 package es.fplumara.dam1.talleres.service.impl;
 
-public class UsuarioServiceImpl {
+import es.fplumara.dam1.talleres.model.Usuario;
+import es.fplumara.dam1.talleres.repository.TallerRepository;
+import es.fplumara.dam1.talleres.repository.UserRepository;
+import es.fplumara.dam1.talleres.service.UsuarioService;
+
+import java.util.List;
+
+public class UsuarioServiceImpl implements UsuarioService {
+
+
+    private UserRepository UserRepository;
+
+    public UsuarioServiceImpl(UserRepository userRepository) {
+        UserRepository = userRepository;
+    }
+
+    @Override
+    public Usuario createUsuario(Usuario CrearUsuario) {
+        return null;
+    }
+
+    @Override
+    public List<Usuario> listarusuario() {
+        return List.of();
+    }
+
+    @Override
+    public Usuario obtenerUsuario(Long id) {
+        return null;
+    }
+
+    @Override
+    public Usuario actualizarUsuario(Long id, Usuario cambiosUsuario) {
+        return null;
+    }
+
+    @Override
+    public Usuario eliminarUsuario(Long id) {
+        return null;
+    }
 }
